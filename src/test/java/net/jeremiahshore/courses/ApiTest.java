d89d84f1c7063b658881be8ea3b976e0ba61e969
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 12:56:56 -0700

    - added test to confirm created response when POSTing course - added test to confirm retrieve course by id - added test to confirm not found response when course is not found by id

--- src/test/java/net/jeremiahshore/courses/ApiTest.java
+++ src/test/java/net/jeremiahshore/courses/ApiTest.java
@@ -1,18 +1,33 @@
 package net.jeremiahshore.courses;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.google.gson.Gson;
+import net.jeremiahshore.courses.dao.CourseDao;
+import net.jeremiahshore.courses.dao.Sql2oCourseDao;
+import net.jeremiahshore.courses.dao.Sql2oCourseDaoTest;
+import net.jeremiahshore.courses.exc.DaoException;
+import net.jeremiahshore.courses.model.Course;
+import net.jeremiahshore.testing.ApiClient;
+import net.jeremiahshore.testing.ApiResponse;
+import org.junit.*;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import spark.Spark;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class ApiTest {
 
     private static final String TEST_PORT = "4568";
     private static final String TEST_DATASOURCE = "jdbc:h2:mem:testing";
     private Connection connection;
+    private CourseDao courseDao;
+    private ApiClient client;
+    private Gson gson;
+    private Course course;
+
 
     @BeforeClass
     public static void startServer() {
@@ -23,7 +38,39 @@ public class ApiTest {
     @Before
     public void setUp() {
         Sql2o sql2o = Api.getConfiguredSql2o(TEST_DATASOURCE);
+        courseDao = new Sql2oCourseDao(sql2o);
         connection = sql2o.open();
+        client = new ApiClient("http://localhost:" + TEST_PORT);
+        gson = new Gson();
+        course = Sql2oCourseDaoTest.createTestCourse();
+    }
+
+    @Test
+    public void addingCoursesReturnsCreatedStatus() {
+        Map<String, String> values = new HashMap<>();
+        values.put("name", "Test");
+        values.put("url", "http://test.com");
+
+        ApiResponse response = client.request("POST", "/courses", gson.toJson(values));
+
+        assertEquals(201, response.getStatus());
+    }
+
+    @Test
+    public void coursesCanBeAccessedById() throws DaoException {
+        courseDao.add(course);
+
+        ApiResponse response = client.request("GET", "/courses/" + course.getId());
+        Course retrievedCourse = gson.fromJson(response.getBody(), Course.class);
+
+        assertEquals(course, retrievedCourse);
+    }
+
+    @Test
+    public void missingCoursesReturnNotFoundStatus() {
+        ApiResponse response = client.request("GET", "/courses/42");
+
+        assertEquals(404, response.getStatus());
     }
 
     @After
