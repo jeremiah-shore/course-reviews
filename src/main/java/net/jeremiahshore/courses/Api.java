d27895974485988083591a9c2978667cd353602c
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 15:11:45 -0700

    refactored .processArgs() to clarify flow of what settings are changed

--- src/main/java/net/jeremiahshore/courses/Api.java
+++ src/main/java/net/jeremiahshore/courses/Api.java
@@ -17,9 +17,9 @@ public class Api {
     private static final String JSON_CONTENT_TYPE = "application/json";
     private static final String DEFAULT_DATASOURCE = "jdbc:h2:~/reviews.db";
     private static final String INIT_SCRIPT_PATH = ";INIT=RUNSCRIPT from 'classpath:db/init.sql'";
+    private static final Gson GSON = new Gson();
 
-    private static Gson gson = new Gson();
-    private static String datasource = DEFAULT_DATASOURCE;
+    private static String datasource;
 
     public static void main(String[] args) {
         processArgs(args);
@@ -33,13 +33,16 @@ public class Api {
     }
 
     private static void processArgs(String[] args) {
-        if(args.length > 0) {
+        if(args.length == 0) {
+            datasource = DEFAULT_DATASOURCE;
+        } else {
             if(args.length != 2) {
                 System.out.println("java Api <port> <datasource>");
                 System.exit(0);
+            } else {
+                port(Integer.parseInt(args[0]));
+                datasource = args[1];
             }
-            port(Integer.parseInt(args[0]));
-            datasource = args[1];
         }
     }
 
@@ -49,14 +52,14 @@ public class Api {
 
     private static void defineCourseHttpMethods(CourseDao courseDao) {
         post("/courses", JSON_CONTENT_TYPE, (request, response) -> {
-            Course course = gson.fromJson(request.body(), Course.class);
+            Course course = GSON.fromJson(request.body(), Course.class);
             courseDao.add(course);
             response.status(201);
             return course;
-        }, gson::toJson); //response transform
+        }, GSON::toJson); //response transform
 
         get("/courses", JSON_CONTENT_TYPE,
-                (request, response) -> courseDao.findAll(), gson::toJson);
+                (request, response) -> courseDao.findAll(), GSON::toJson);
 
         get("/courses/:id", JSON_CONTENT_TYPE, (request, response) -> {
             int id = Integer.parseInt(request.params("id"));
@@ -65,7 +68,7 @@ public class Api {
                 throw new ApiError(404, "Could not find course with id " + id);
             }
             return course;
-        }, gson::toJson);
+        }, GSON::toJson);
     }
 
     private static void defineReviewHttpMethods() {
@@ -81,7 +84,7 @@ public class Api {
 
             response.type(JSON_CONTENT_TYPE); //after does not run in .exception()
             response.status(error.getStatus());
-            response.body(gson.toJson(jsonMap));
+            response.body(GSON.toJson(jsonMap));
         });
     }
 
